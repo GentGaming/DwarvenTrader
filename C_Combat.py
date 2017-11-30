ec2deaef0f10166e5dbfc0f8a20a143ce69cb78c
GentGaming <athenseed1@gmail.com>
2017-11-30 12:36:27 -0800

    I hate that console that pops up

--- C_Combat.py
+++ C_Combat.py
@@ -1,2 +1,12 @@
 class Combat:
-    pass
+    def __init__(combatants):
+        pass
+    def establishInitiative(self):
+        pass
+    def dealDamage(self):
+        pass
+    def calculateDamage(self):
+        pass
+    def autoCombat(self):
+        pass
+    
