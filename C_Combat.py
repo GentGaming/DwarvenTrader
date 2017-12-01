c42cf3e544642dec8b913d66af8b38fd5faf0d8d
rckohler <fearsbane@yahoo.com>
2017-12-01 11:53:16 -0800

    a2

--- C_Combat.py
+++ C_Combat.py
@@ -6,18 +6,29 @@ class Combat:
     def establishInitiative(self):
         goodguyinitiative = 1
         badguyinitiative = 2
+        #incomplete
         initiativelist = [goodguyinitiative,badguyinitiative]
         return initiativelist
     def updateCombat(self):
-        health=health-damage
-        return health
+        pass
+        #health=health-damage
+        #removed above correct code would be health -= damage if health and damage were defined.
+        #return health
+        #should not be called update combat and just change health.
     def calculateDamage(self):
+        #this should live in CombatUnit
+        '''
         i=0
         damage=0
         while i < 3:
             damage+=random.randint(1,6)
         print(damage)
         return damage
+        '''
     def autoCombat(self):
+        '''
+        the % does not work like this.
         while health < 15%:
+
             flee
+        '''
