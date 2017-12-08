52a4cd15b161c1bcfa637250f878b9f4e1ab2967
rckohler <fearsbane@yahoo.com>
2017-12-08 11:30:16 -0800

    a

--- C_CombatOption.py
+++ C_CombatOption.py
@@ -7,16 +7,33 @@ class CombatOption(ListObject):
     CRIPPLED = 4
     WOUNDED = 5
 
-    def __init__(self,name, attack,defense,damageMod,freeMove = False,forwardMove = False, awayMove = False,statusesAdded = [], statusesRemoved = []):
+    def __init__(self,name, attack,defense,speedMod = 0, delayMod =0, damageMod = 0,freeMove = False,forwardMove = False, awayMove = False, push = False, pull = False, statusesAdded = [], statusesRemoved = []):
         self.attackMod = attack
+        self.name = name
         self.defenseMod = defense
         self.damageMod = damageMod
+        self.speedMod = speedMod
+        self.delayMod = delayMod
         self.freeMove = freeMove
         self.forwardMove = forwardMove
         self.awayMove = awayMove
         self.statusesAdded = statusesAdded
         self.statusesRemoved = statusesRemoved
 
+
+#-2 only self polearm v  opponent grapple
+PUSH = CombatOption("Push",1,1,0,0,0,False,False,False,True)
+BACKSTEP = CombatOption("Backstep",-2,-2,0,0,0,False,False,True)
+#-1
+
+#0
+
+#1
+
+#2
+
+#3
+#empty because if only lives with three range weapons..
 
 LUNGE = CombatOption("Lunge",0,-2,1,False,True,False)
 PARRY = CombatOption("Parry",-4,2,-3,True,False,False)
